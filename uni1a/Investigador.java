3c9a590b37ce1deb6e6ca8d36af1216b783b67ab
Alex <166135715+PastFlex1@users.noreply.github.com>
2025-08-03 23:30:18 -0500

    Update Investigador.java

--- uni1a/Investigador.java
+++ uni1a/Investigador.java
@@ -3,42 +3,31 @@ package uni1a;
 public class Investigador {
     private String nombre;
     private String especialidad;
-    private String institucion;
 
-    public Investigador(String nombre, String especialidad, String institucion) {
+    public Investigador(String nombre, String especialidad) {
         this.nombre = nombre;
         this.especialidad = especialidad;
-        this.institucion = institucion;
     }
 
-    // Getters
+    // Getters y setters
     public String getNombre() {
         return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
     }
 
     public String getEspecialidad() {
         return especialidad;
     }
 
-    public String getInstitucion() {
-        return institucion;
-    }
-
-    // Setters
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
     public void setEspecialidad(String especialidad) {
         this.especialidad = especialidad;
     }
 
-    public void setInstitucion(String institucion) {
-        this.institucion = institucion;
-    }
-
     @Override
     public String toString() {
-        return "Investigador: " + nombre + " (Especialidad: " + especialidad + ", Institución: " + institucion + ")";
+        return nombre + " (" + especialidad + ")";
     }
 }
