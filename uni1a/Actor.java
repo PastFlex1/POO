1d91163105619fc582de3126865f815313b2d07b
Alex <166135715+PastFlex1@users.noreply.github.com>
2025-08-03 23:29:08 -0500

    Actor

--- uni1a/Actor.java
+++ uni1a/Actor.java
@@ -2,43 +2,33 @@ package uni1a;
 
 public class Actor {
     private String nombre;
-    private String fechaNacimiento; // Considerar usar java.time.LocalDate para una mejor gestión de fechas
-    private String nacionalidad;
+    private String paisOrigen;
 
-    public Actor(String nombre, String fechaNacimiento, String nacionalidad) {
+    public Actor(String nombre, String paisOrigen) {
         this.nombre = nombre;
-        this.fechaNacimiento = fechaNacimiento;
-        this.nacionalidad = nacionalidad;
+        this.paisOrigen = paisOrigen;
     }
 
-    // Getters
+    // Getters y setters
     public String getNombre() {
         return nombre;
     }
 
-    public String getFechaNacimiento() {
-        return fechaNacimiento;
-    }
-
-    public String getNacionalidad() {
-        return nacionalidad;
-    }
-
-    // Setters
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
 
-    public void setFechaNacimiento(String fechaNacimiento) {
-        this.fechaNacimiento = fechaNacimiento;
+    public String getPaisOrigen() {
+        return paisOrigen;
     }
 
-    public void setNacionalidad(String nacionalidad) {
-        this.nacionalidad = nacionalidad;
+    public void setPaisOrigen(String paisOrigen) {
+        this.paisOrigen = paisOrigen;
     }
 
     @Override
     public String toString() {
-        return "Actor: " + nombre + " (Nacimiento: " + fechaNacimiento + ", Nacionalidad: " + nacionalidad + ")";
+        return nombre + " (" + paisOrigen + ")";
     }
 }
+
